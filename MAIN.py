a9816770b98d4b7893317ce809668bab355aec98
syed ahmed raza <syedahmed.raza@student.ucll.be>
2025-12-15 13:55:01 +0100

    MAIN

---
+++ MAIN.py
@@ -0,0 +1,12 @@
+def main ():
+    import pygame
+    def create_main_surface ():
+        while 1==1 : 
+            # Initialize Pygame
+            pygame.init()
+
+            # Tuple representing width and height in pixels
+            screen_size = (1024, 768)
+
+            # Create window with given size
+            pygame.display.set_mode(screen_size) 
